cdcf24e0d7d782e28222237e1aea08057b508d04
EngRobot33 <hmdkhsrvee@gmail.com>
2024-04-26 15:07:32 +0330

    [ADD] image processor app

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+DJANGO_APPS = [
+    'image_processor.apps.ImageProcessorConfig',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    *DJANGO_APPS,
 ]
 
 MIDDLEWARE = [
