75be61128c6e4da597d9bada1549ee1dc7ff24d5
EngRobot33 <hmdkhsrvee@gmail.com>
2024-04-28 18:42:29 +0330

    [ADD] TRACER_B7_MODEL_FILE_PATH

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TRACER_B7_MODEL_FILE_PATH = config('TRACER_B7_MODEL_FILE_PATH')
